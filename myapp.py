e3716fe54fe5e5abdf8a0978d0c8b0d59d47ff20
Alkiviadis Papadopoulos <alkiviadis.papadopoulos@cern.ch>
2020-01-08 14:25:49 +0200

    bug fix

--- myapp.py
+++ myapp.py
@@ -5,13 +5,17 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-from flask import Flask
+from flask import Flask, redirect, url_for 
 
 app = Flask(__name__, static_folder="./static/")
 
 
 @app.route('/')
-def hello_world():
+def index():
+    return redirect(url_for('static', filename='index.html'))
+
+@app.route('/test')
+def test():
     cur = conn.cursor()
     cur.execute("SELECT * FROM test;")
     return "\n".join(str(x) for x in cur.fetchall())
