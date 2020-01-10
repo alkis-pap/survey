e1af5508bfe6a112086f6db8937be28e854cc0b0
Alkiviadis Papadopoulos <alkiviadis.papadopoulos@cern.ch>
2020-01-10 12:24:41 +0100

    ~

--- myapp.py
+++ myapp.py
@@ -1,18 +1,41 @@
+# import json
+
+# surveys
+
+# with open("static/survey.json") as f:
+#     survey = json.load(f)
+
+# question_types = ["matrix"]
+
+# n_questions = sum(1 for page in survey["pages"] for question in page["questions"] if question["type"] in question_types)
+n_questions = 2
+
 import os
 import psycopg2
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
 
-from flask import Flask, redirect, url_for 
+from flask import Flask, redirect, url_for, send_from_directory
+import random
 
 app = Flask(__name__, static_folder="./static/")
-
 
 @app.route('/')
 def index():
     return redirect(url_for('static', filename='index.html'))
+
+@app.route('/survey.json'):
+def survey():
+    return send_from_directory('surveys', filename='survey{}.json'.format(random.randint(1, 4)))
+
+@app.route('/update', methods=['POST'])
+def post():
+    data = request.get_json()
+    if len(data) == n_questions:
+        cur.execute("INSERT INTO results VALUES (NOW()::timestamp, " + ",".join(["%d"] * n_questions) + ")", (data[key] for key in data))
 
 @app.route('/test')
 def test():
--- static/index.js
+++ static/index.js
@@ -8,18 +8,18 @@ var widget = {
     //If the widgets depends on third-party library(s) then here you may check if this library(s) is loaded
     widgetIsLoaded: function () {
         //return typeof $ == "function" && !!$.fn.select2; //return true if jQuery and select2 widget are loaded on the page
-        return true; //we do not require anything so we just return true. 
+        return true; //we do not require anything so we just return true.
     },
     //SurveyJS library calls this function for every question to check, if it should use this widget instead of default rendering/behavior
     isFit: function (question) {
         //we return true if the type of question is textwithbutton
-        return question.getType() === 'textwithbutton';
+        return question.getType() === "textwithbutton";
         //the following code will activate the widget for a text question with inputType equals to date
         //return question.getType() === 'text' && question.inputType === "date";
     },
     //Use this function to create a new class or add new properties or remove unneeded properties from your widget
     //activatedBy tells how your widget has been activated by: property, type or customType
-    //property - it means that it will activated if a property of the existing question type is set to particular value, for example inputType = "date" 
+    //property - it means that it will activated if a property of the existing question type is set to particular value, for example inputType = "date"
     //type - you are changing the behaviour of entire question type. For example render radiogroup question differently, have a fancy radio buttons
     //customType - you are creating a new type, like in our example "textwithbutton"
     activatedByChanged: function (activatedBy) {
@@ -28,7 +28,7 @@ var widget = {
         Survey.JsonObject.metaData.addClass("textwithbutton", [], null, "text");
         //signaturepad is derived from "empty" class - basic question class
         //Survey.JsonObject.metaData.addClass("signaturepad", [], null, "empty");
-  
+
         //Add new property(s)
         //For more information go to https://surveyjs.io/Examples/Builder/?id=addproperties#content-docs
         Survey.JsonObject.metaData.addProperties("textwithbutton", [
@@ -52,23 +52,23 @@ var widget = {
         button.innerText = question.buttonText;
         button.onclick = function () {
             question.value = "You have clicked me";
-        }
-  
+        };
+
         //set the changed value into question value
         text.onchange = function () {
             question.value = text.value;
-        }
+        };
         var onValueChangedCallback = function () {
             text.value = question.value ? question.value : "";
-        }
-        var onReadOnlyChangedCallback = function() {
-          if (question.isReadOnly) {
-            text.setAttribute('disabled', 'disabled');
-            button.setAttribute('disabled', 'disabled');
-          } else {
-            text.removeAttribute("disabled");
-            button.removeAttribute("disabled");
-          }
+        };
+        var onReadOnlyChangedCallback = function () {
+            if (question.isReadOnly) {
+                text.setAttribute("disabled", "disabled");
+                button.setAttribute("disabled", "disabled");
+            } else {
+                text.removeAttribute("disabled");
+                button.removeAttribute("disabled");
+            }
         };
         //if question becomes readonly/enabled add/remove disabled attribute
         question.readOnlyChangedCallback = onReadOnlyChangedCallback;
@@ -78,7 +78,6 @@ var widget = {
         onValueChangedCallback();
         //make elements disabled if needed
         onReadOnlyChangedCallback();
-  
     },
     //Use it to destroy the widget. It is typically needed by jQuery widgets
     willUnmount: function (question, el) {
@@ -87,130 +86,25 @@ var widget = {
         //var $el = $(el).find("select");
         //$el.data('picker').destroy();
     }
-  }
-  
-  //Register our widget in singleton custom widget collection
-  Survey.CustomWidgetCollection.Instance.addCustomWidget(widget, "customtype");
+};
+
+//Register our widget in singleton custom widget collection
+Survey.CustomWidgetCollection.Instance.addCustomWidget(widget, "customtype");
 
 Survey.StylesManager.applyTheme("modern");
 
-var json = {
-    pages: [
-        {
-            questions: [
-                {
-                    type: "textwithbutton",
-                    name: "q1",
-                    placeHolder: "put some text here",
-                    buttonText: "Custom button text"
-        
-                },
-                {
-                    type: "matrix",
-                    name: "Quality",
-                    title:
-                        "Please indicate if you agree or disagree with the following statements",
-                    columns: [
-                        {
-                            value: 1,
-                            text: "Strongly Disagree"
-                        },
-                        {
-                            value: 2,
-                            text: "Disagree"
-                        },
-                        {
-                            value: 3,
-                            text: "Neutral"
-                        },
-                        {
-                            value: 4,
-                            text: "Agree"
-                        },
-                        {
-                            value: 5,
-                            text: "Strongly Agree"
-                        }
-                    ],
-                    rows: [
-                        {
-                            value: "affordable",
-                            text: "Product is affordable"
-                        },
-                        {
-                            value: "does what it claims",
-                            text: "Product does what it claims"
-                        },
-                        {
-                            value: "better then others",
-                            text: "Product is better than other products on the market"
-                        },
-                        {
-                            value: "easy to use",
-                            text: "Product is easy to use"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            questions: [
-                {
-                    type: "matrix",
-                    name: "Quality2",
-                    title:
-                        "Please indicate if you agree or disagree with the following statements",
-                    columns: [
-                        {
-                            value: 1,
-                            text: "Strongly Disagree"
-                        },
-                        {
-                            value: 2,
-                            text: "Disagree"
-                        },
-                        {
-                            value: 3,
-                            text: "Neutral"
-                        },
-                        {
-                            value: 4,
-                            text: "Agree"
-                        },
-                        {
-                            value: 5,
-                            text: "Strongly Agree"
-                        }
-                    ],
-                    rows: [
-                        {
-                            value: "affordable",
-                            text: "Product is affordable"
-                        },
-                        {
-                            value: "does what it claims",
-                            text: "Product does what it claims"
-                        },
-                        {
-                            value: "better then others",
-                            text: "Product is better than other products on the market"
-                        },
-                        {
-                            value: "easy to use",
-                            text: "Product is easy to use"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-};
+$.getJSON("survey.json", function (json) {
+    window.survey = new Survey.Model(json);
 
-window.survey = new Survey.Model(json);
+    survey.onComplete.add(function (result) {
+        $.ajax("update", {
+            data : JSON.stringify(result.data),
+            contentType : 'application/json',
+            type : 'POST'
+        });
+        document.querySelector("#surveyResult").textContent =
+            "Result JSON:\n" + JSON.stringify(result.data, null, 3);
+    });
 
-survey.onComplete.add(function (result) {
-    document.querySelector("#surveyResult").textContent =
-        "Result JSON:\n" + JSON.stringify(result.data, null, 3);
+    $("#surveyElement").Survey({ model: survey });
 });
-
-$("#surveyElement").Survey({ model: survey });
