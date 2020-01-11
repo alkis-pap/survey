b96218c3114c2116b8b94a726e72e402feaf1724
Alkiviadis Papadopoulos <alkiviadis.papadopoulos@cern.ch>
2020-01-11 23:02:11 +0100

    removed heroku stuff

--- backend.py
+++ backend.py
@@ -10,13 +10,11 @@
 # n_questions = sum(1 for page in survey["pages"] for question in page["questions"] if question["type"] in question_types)
 n_questions = 2
 
-import os
-import psycopg2
+import sqlite3
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = sqlite3.connect('data.db')
 cur = conn.cursor()
+
 
 from flask import Flask, redirect, url_for, send_from_directory
 import random
@@ -31,14 +29,14 @@ def index():
 def survey():
     return send_from_directory('surveys', filename='survey{}.json'.format(random.randint(1, 4)))
 
-@app.route('/update', methods=['POST'])
-def post():
-    data = request.get_json()
-    if len(data) == n_questions:
-        cur.execute("INSERT INTO results VALUES (NOW()::timestamp, " + ",".join(["%d"] * n_questions) + ")", (data[key] for key in data))
+# @app.route('/update', methods=['POST'])
+# def post():
+#     data = request.get_json()
+#     if len(data) == n_questions:
+#         cur.execute("INSERT INTO results VALUES (NOW()::timestamp, " + ",".join(["%d"] * n_questions) + ")", (data[key] for key in data))
 
-@app.route('/test')
-def test():
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM test;")
-    return "\n".join(str(x) for x in cur.fetchall())
+# @app.route('/test')
+# def test():
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM test;")
+#     return "\n".join(str(x) for x in cur.fetchall())
